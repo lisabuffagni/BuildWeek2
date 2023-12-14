364e737f15cb5d00013e07dd23b696182588c719
VczEno <arpaia.vin@gmail.com>
2023-12-14 17:34:01 +0100

    sidebar & player

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let closeFaBtn= document.querySelector('#FaCloseBtn')
+console.log(closeFaBtn)
+let sidebar= document.querySelector('aside')
+
+
+
+    closeFaBtn.addEventListener('click', () => sidebar.classList.add('d-none'))
+
+
